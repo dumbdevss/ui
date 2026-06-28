e8b9afe587e58a28e918848b01e3522bd37cff94
xxCodexIAxx <sadani012001@gmail.com>
2026-06-28 13:39:22 -0600

    Export AssetPill from public components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -18,6 +18,7 @@
  */
 
 // Export all components
+export { AssetBadge, AssetPill } from './AssetBadge';
 export { SorobanPanel } from './SorobanPanel';
 export { TransactionPanel } from './TransactionPanel';
 export { ErrorBoundary } from './ErrorBoundary';
